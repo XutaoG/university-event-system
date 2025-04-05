08b9246bc22494b982f0aa8a48e9aafc4ea261ec
Xutao Gao <taogeeee@gmail.com>
2025-04-04 21:33:21 -0400

    Add home page folder design

--- college-event-website-frontned/src/app/components/home/home.component.ts
+++ college-event-website-frontned/src/app/components/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+	selector: 'app-home',
+	imports: [RouterOutlet, MatIconModule, CommonModule],
+	templateUrl: './home.component.html',
+	styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+	test() {
+		console.log('TEST');
+	}
 }
